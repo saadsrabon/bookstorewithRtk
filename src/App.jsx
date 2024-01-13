d1b6648a005c86e7d442e15152a80372e7689e12
Saad Srabon <saadsrabon2@gmail.com>
2024-01-14 03:28:23 +0600

    2024-01-14 03:28:23 - set is Featured added

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,17 @@ import EditBooks from './pages/EditBooks'
 import { Layout } from './layout/Layout'
 import { useState } from 'react'
 
-function App() {
-  const [isFeatured, setIsFeatured] = useState("");
 
+function App() {
+
+  const [isFeatured, setIsFeatured] = useState("");
   return (
     <>
      <BrowserRouter>
-     <Layout setIsFeatured={setIsFeatured}>
+     <Layout >
      <Routes>
      
-      <Route path="/" element={<Home isFeatured={isFeatured} />} />
+      <Route path="/" element={<Home isFeatured={isFeatured} setIsFeatured={setIsFeatured} />} />
         <Route path="/AddBook" element={<AddBook />} />
         <Route path="/EditBook" element={<EditBooks />} />
      
