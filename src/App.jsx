4ee3b03ffa23cb0ad217f342691f5d4189319041
Saad Srabon <saadsrabon2@gmail.com>
2024-01-14 02:26:57 +0600

    home page initalized

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { Home } from './pages/Home'
+import AddBook from './pages/AddBook'
+import EditBooks from './pages/EditBooks'
+import { Layout } from './layout/Layout'
 
 function App() {
 
 
   return (
     <>
+     <BrowserRouter>
+     <Layout>
+     <Routes>
      
+      <Route path="/" element={<Home />} />
+        <Route path="/AddBook" element={<AddBook />} />
+        <Route path="/EditBook" element={<EditBooks />} />
+     
+        
+
+        </Routes>
+        </Layout>
+     </BrowserRouter>
     </>
   )
 }
