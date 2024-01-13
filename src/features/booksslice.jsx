38d28a43eac22b64f052db5da33937b753c6243c
Saad Srabon <saadsrabon2@gmail.com>
2024-01-14 02:48:35 +0600

    2024-01-14 02:48:35 - api slice ready

--- src/features/booksslice.jsx
+++ src/features/booksslice.jsx
@@ -1,5 +1,36 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const booksSlice = createSlice({
-
+const booksApi = createApi({
+    reducerPath:"books",
+    baseQuery: fetchBaseQuery({baseUrl: "http://localhost:3001"}),
+    tagTypes: ["Books"], // this is for caching and white listing tags
+    endpoints: (builder) => ({
+        getAllBooks: builder.query({
+            query: () => "/books",
+            providesTags: ["Books"],
+        }),
+        addBook: builder.mutation({
+            query: (book) => ({
+                url: "/books",
+                method: "POST",
+                body: book,
+            }),
+            invalidatesTags: ["Books"],
+        }),
+        deleteBook: builder.mutation({
+            query: (id) => ({
+                url: `/books/${id}`,
+                method: "DELETE",
+            }),
+            invalidatesTags: ["Books"],
+        }),
+        editBook: builder.mutation({
+            query: ({ id, ...patch }) => ({
+                url: `/books/${id}`,
+                method: "PATCH",
+                body: patch,
+            }),
+            invalidatesTags: ["Books"],
+        }),
+    }),
 });
