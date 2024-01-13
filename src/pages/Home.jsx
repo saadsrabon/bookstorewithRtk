e0984a2a58d0a889ab96b91c313e57b965245d9b
Saad Srabon <saadsrabon2@gmail.com>
2024-01-14 03:15:06 +0600

    2024-01-14 03:15:06 - main div added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -67,7 +67,7 @@ export const Home = () => {
 
   return (
     <main className="py-12 px-6 2xl:px-6 container">
-    <div className="order-2 xl:-order-1">
+    {/* <div className="order-2 xl:-order-1">
       <div className="flex items-center justify-between mb-12">
         <h4 className="mt-2 text-xl font-bold">Book List</h4>
 
@@ -84,7 +84,7 @@ export const Home = () => {
 
 
       </div>
-    </div>
+    </div> */}
   </main>
   )
 }
